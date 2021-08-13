37a5adea5c08f26e233157c90a0f318446028b9c
Zachary Smith <22395357+Cutwell@users.noreply.github.com>
2021-08-14 00:12:33 +0100

    v1.0
    * insert UI button to redirect user to gtihub1s webpage from github repositories

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # github1s-chrome-extension
  Add UI to open GitHub repositories in GitHub1s
+
+* A simple Chrome extension, which should be compatible with any Chromium-based browser(1). 
+* Inserts additional UI content into the repository navigation bar, providing a redirect to the [GitHub1s](https://github1s.com/conwnet/github1s) repository-viewer webapp.
+* Note: should only insert UI elements on repository pages, but code will attempt to insert into the first  "file-navigation" class element on each github.com domain page, which may cause unexpected UI if this class occurs elsewhere on the website than the repository pages (2).
+
+
+(1) tested in Opera.
+
+(2) however, no such problems where encountered in testing.
+
+## License
+MIT
---
+++ data/js/content.js
@@ -0,0 +1,68 @@
+// insert custom css for button //
+// create an element
+const style = document.createElement('style');
+
+// add CSS styles
+style.innerHTML = `
+    .github1s {
+        display: block;
+        margin-left: 8px;
+        background-color: #fafbfc;
+        border-color: rgba(27, 31, 35, 0.15)!important;
+        box-shadow: rgba(27, 31, 35, 0.04), rgba(255, 255, 255, 0.25);
+        color: #24292e;
+        transition: .2s cubic-bezier(.3,0,.5,1)!important;
+        transition-property: color,background-color,border-color;
+        appearance: none;
+        border: 1px solid;
+        border-radius: 6px;
+        cursor: pointer;
+        font-size: 14px;
+        font-weight: 500;
+        line-height: 20px;
+        padding: 5px 16px;
+        position: relative;
+        user-select: none;
+        vertical-align: middle;
+        white-space: nowrap;
+        text-decoration: none!important;
+        box-sizing: border-box;
+        word-wrap: break-word;
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji;
+    }
+    .github1s:hover {
+        background-color: #f3f4f6;
+        border-color: rgba(27, 31, 35, 0.15)!important;
+        transition-duration: .1s;
+        outline-width: 0;
+        text-decoration: none;
+    }
+`;
+
+// append to DOM
+document.head.appendChild(style);
+
+// contruct the additional button
+const a_element = document.createElement('a');
+
+a_element.classList.add("github1s")
+
+let a_text = document.createTextNode("Open in GitHub1s");
+a_element.appendChild(a_text);
+
+// add onclick redirect
+a_element.onclick = function() {
+    window.location.href = window.location.href.replace(
+        "github.com",
+        "github1s.com"
+    );
+}
+
+// insert into DOM //
+// locate file navigation
+let nav = document.getElementsByClassName("file-navigation")[0];
+
+// insert element
+if (nav !== undefined) {
+    nav.append(a_element);
+}
